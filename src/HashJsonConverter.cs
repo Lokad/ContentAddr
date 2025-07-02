955be38b077c8a47eb786982867ed41e77fe3883
Victor Nicollet <victor.nicollet@lokad.com>
2025-07-02 10:53:44 +0200

    Move to .NET 8.0 and implement Hash converter for System.Text.Json

---
+++ src/HashJsonConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Lokad.ContentAddr;
+
+/// <summary> Passthrough converter, only write a json string </summary>
+/// <remarks>
+///     This is necessary because System.Text.Json does not honor the existence
+///     of a TypeConverter to string.
+/// </remarks>
+public class HashJsonConverter : JsonConverter<Hash>
+{
+    public override Hash Read(
+        ref System.Text.Json.Utf8JsonReader reader,
+        Type typeToConvert,
+        System.Text.Json.JsonSerializerOptions options)
+    =>
+        new(reader.GetString() ?? throw new ArgumentException("Missing string", nameof(reader)));
+
+    public override void Write(System.Text.Json.Utf8JsonWriter writer, Hash value, System.Text.Json.JsonSerializerOptions options) =>
+        writer.WriteStringValue(value.ToString());
+}
